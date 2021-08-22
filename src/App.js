83de17ded2b23d5d95d223c7745b1571aec85e3b
Teodor Solca <t-c.solca@metro.digital>
2021-08-22 22:29:50 +0300

    Changed index.html

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './components/_settings/_base.css';
 import styles from './App.css';
-import DicesContainer from './components/templates/Dices';
 import Board from './components/pages/Board';
 
 function App() {
   return (
     <div className={styles.app}>
       <Board />
-      {/* <DicesContainer /> */}
     </div>
   );
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,5 @@
 const { merge } = require('webpack-merge');
+// eslint-disable-next-line import/extensions
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
